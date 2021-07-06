4a7abaf0f9194f5a78fe748dad7aa2049d5247f5
gabbyklein1 <gabbyloklein@gmail.com>
2021-07-06 15:29:13 -0400

    progress on tab 4

--- Dash/apps/app2.py
+++ Dash/apps/app2.py
@@ -1,26 +1,116 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+import dash_table
 
 from app import app
 
-layout = html.Div([
-    html.H3('App 2'),
-    dcc.Dropdown(
-        id='app-2-dropdown',
-        options=[
-            {'label': 'App 2 - {}'.format(i), 'value': i} for i in [
-                'Why?', 'Why Not?', 'Dummy or Smarty?'
-            ]
-        ]
-    ),
-    html.Div(id='app-2-display-value'),
-    dcc.Link('Go to App 1', href='/apps/app1')
+SIDEBAR_STYLE = {"position": "fixed", "top": 0, "left": 0,"bottom": 0,
+    "width": "16rem", "padding": "2rem 1rem","background-color": "#f8f9fa",}
+
+
+CONTENT_STYLE = {"margin-left": "18rem", "margin-right": "2rem", "padding": "2rem 1rem",}
+Padded_STYLE = {"margin-left": "-1rem", "margin-right": "2rem","padding": "2rem 1rem",}
+
+sidebar = html.Div([
+        html.H3("Housing Prices in Ames, Iowa", style={'textAlign': 'center'}),
+        html.Hr(),
+        dbc.Nav(
+            [
+                dbc.NavLink("Introduction", href="/apps/app1", active="exact"),
+                dbc.NavLink("Project", href="/apps/app2", active="exact"),
+                dbc.NavLink("Pricing Model", href="/apps/app3", active="exact"),
+                dbc.NavLink("Real Estate Handbook", href="/apps/app4", active="exact"),
+                dbc.NavLink("Miscellaneous", href="/apps/app5", active="exact"),
+            ], vertical=True, pills=True
+        )
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Tab content 1')
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
+
+
+
+## Content is your new 'layout'
+
+
+content = html.Div([
+
+    html.Div([
+        html.Div([html.H3('Project Overview'),
+    dcc.Tabs(id='tabs-example', value='tab-1', children=[
+        dcc.Tab(label='Background', value='tab-1'),
+        dcc.Tab(label='Models', value='tab-2'),
+    ]),
+    html.Div(id='tabs-example-content', style=Padded_STYLE)
 ])
+
+], style=CONTENT_STYLE)])
+
+
+## This is how the sidebar and content is displayed together
+layout = html.Div([sidebar, content])
+
+df=pd.read_csv('All_housess_coords_4map.csv')
+fig=px.scatter_mapbox(df, lat="Lat", lon="Long")
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+
+model_table=pd.DataFrame({'Model':['Lasso','Stepwise','Random Forest','SVR'],'Features Used':['Lasso','Stepwise','Tree','SVM'],'Train R2':['Lasso','Stepwise','Tree','SVM'],'Test R2':['Lasso','Stepwise','Tree','SVM']},index=['Lasso','Stepwise','Random Forest','SVM'])
+
 
 
 @app.callback(
     Output('app-2-display-value', 'children'),
     Input('app-2-dropdown', 'value'))
+@app.callback(Output('tabs-example-content', 'children'),
+    Input('tabs-example', 'value'))
 def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    if value=='tab-1':
+        return (html.Div([
+            html.H3('Welcome To Ames, Iowa:',style={'font-weight': 'bold'}),
+            ]),dcc.Markdown('''For this assignment, we analyzed a data set of housing prices in and around Ames, Iowa. With over 2,500 rows of data pertaining to house sale records between 2006 - 2010, we took it upon ourselves to thoroughly clean, analyze, and create machine learning models to answer a handful of questions our team found beneficial from both a business and consumer perspective. This dataset includes numerous features to work with in order to come to the most optimal resolutions to the questions we looked to resolve. As visualized in the map below, the area of housing that our data focuses on is around Iowa State University.
+            '''),
+            dcc.Graph(id="choropleth",figure=fig),
+            html.Hr(),html.Div([
+            html.H3('Our Business Angle:',style={'font-weight': 'bold'}),]),
+            dcc.Markdown('''We chose to investigate the Ames, Iowa housing market from the perspective of a data scientist working at Zillow Offers, Zillow's service line that purchases homes directly from individuals. Zillow Offers is an instant buying service provided to individual homeowners. Simply fill out a short online form about your home and Zillow will send an offer to purchase in just two business days, contingent upon an inspection. You have five days to accept the offer, which is finalized after an in-person inspection. You don't have to show your home, don't have to prep it for sale, can set your own move-out date, and have surety of payment. Also, if you choose not to sell your home to Zillow you can list it through a Zillow Premier Agent.
+            This is primarily offered as a service to home sellers and is a way to drive volume into Zillow's other businesses. Zillow isn't flipping homes at a significant profit or renovating homes (just performing light, make-ready repairs), and runs its Offers business at 'razor-thin' margins. A 2019 analysis of instant home buyers (competitors include OpenDoor and OfferPAD) showed an average profit over cost of renovations of just 1.3%. Zillow bought 4,162 homes in 2020, with the Offers service currently available in 25 metro areas across the country.
+            Zillow specifically cites its "superior data science and technology" as a competitive advantage, making this business angle chosen for our project particularly relevant. We decided to tackle two significant business problems for Zillow Offers as it hypothetically expands into Ames, Iowa; accurately predicting home prices and maintaining positive relations with the existing real estate industry in the area. The second point can be particularly difficult for Zillow as it continues to make inroads into the home buying industry. To provide incentive for real estate agents to work with Zillow rather than isolate the company, we will assemble a handbook for Zillow Premier Agents that provides useful data-driven insights into the local housing market in Ames, Iowa. This information will help agents provide more value to their clients and encourage them to work with Zillow, not against it.
+            '''))
+    else:
+        return (html.Div([
+            html.H3('Data Preproccessing:',style={'font-weight': 'bold'}),
+            ]),dcc.Markdown('''
+            #### Attribute Creation
+            After cleaning, pruning and dummifying necessary features, we created two columns from the existing data we hypothesized would be more useful in our modeling. One column we created was the ‘RemodelYrsAftBuilt’ which is how long after being built a house was remodeled. If the year it was built was different than the year a house was remodeled, then the house was deemed to be remodeled and this column is the difference between those years.
+            A second feature we created was neighborhood clusters. Instead of dummifying the neighborhood category which had almost 30 unique values, we wanted to extract the most important neighborhoods. However, each neighborhood was unique and we did not want to lose information by combining dissimilar values. We performed hierarchical clustering on each neighborhood as an instance to find which neighborhoods were highly dissimilar and which could be combined. As a result we combined the most similar neighborhoods and formed five clusters. More about this can be found in the Real Estate Handbook.
+            #### Row Selection
+            With so many different rows and columns, as well as various types of information included in our data, a huge task in our cumulative process was creating the most ideal dataset to work with. This meant transforming our data to fit the needs of our research and our machine learning models. For example, when it came to certain features of type categories, we decided to create binarized versions of those columns so that they may be interpretable by our models. As we aimed to be as specific as possible with our research, many columns and rows found themselves to be unnecessary to our cause and therefore were dropped altogether, but not before they received a thorough analysis to confirm their value.
+            #### Dropped Attributes
+            After extensive analysis, one of the few setbacks we experienced happened to revolve around one column: ‘LndAc_S’. A column that was received separate from what was provided to us through Kaggle, this feature showed the highest level of value to our dataset with a feature importance score of 87%. Although its relation to the rest of the data was predominant, due to our lack of knowledge on what it informed us of and multicolinearity, we had no choice but to drop this column entirely.
+            '''), html.Hr(),html.Div([
+            html.H3('Our Models:',style={'font-weight': 'bold'}),
+            ]),dcc.Markdown('''Because we had over 100 attributes after dummifying necessary columns, feature selection was highly important in creating an explainable, properly fit model. To do so we employed Lasso Regression and Stepwise Feature Selection. The Stepwise was most successful in whittling the number of used features while maintaining stellar training and testing R2 values. Below is a summary of the models we tried on our data and their performance. An in depth analysis of the best performing model can be found in the Pricing Model tab.
+            '''),dash_table.DataTable(
+            id='Model_table',
+            columns=[{"name": i, "id": i}
+                 for i in model_table.columns],
+                 data=model_table.to_dict('records'),
+                 style_cell=dict(textAlign='left')))#,
+        #style_header=dict(backgroundColor="paleturquoise"),
+        #style_data=dict(backgroundColor="lavender")
--- Dash/apps/app4.py
+++ Dash/apps/app4.py
@@ -7,7 +7,10 @@ from app import app
 import plotly.express as px
 from KNN_Funct import get_sales_comps
 import dash_table
-tdata=pd.read_csv('apps/tdata_for_realtor_map.csv')
+import sys
+
+
+tdata=pd.read_csv('apps/data/tdata_for_realtor_map.csv')
 
 
 SIDEBAR_STYLE = {"position": "fixed", "top": 0, "left": 0,"bottom": 0,
@@ -66,28 +69,39 @@ def display_value(value):
     Input('tabs', 'value'))
 def display_value(value):
     if value=='tab-1':
-        return html.Div([
-            html.H3('Welcome Realtors',style={'font-weight': 'bold'}),
-            ]),dcc.Markdown('''Introducing our Real Estate Handbook, a data rich review of Ames to help real estate agents better understand the market and further assist customers.
-            Here you will find analysis of different house features' effect on price as found in our model, a look into the similar and different neighborhoods in Ames, as well as an ML generated sales comps tools for your convenience.
-'''),html.Div([html.Img(src='shutterstock_752498263-1400x621.jpg')], style={'textAlign': 'center'}),
+        return (html.Div([
+        html.H3('Welcome Realtors',style={'font-weight': 'bold'}),]),
+        dcc.Markdown('''Introducing our Real Estate Handbook, a data rich review of Ames to help real estate agents better understand the market and further assist customers. Here you will find analysis of different house features effect on price as found in our model, a look into the similar and different neighborhoods in Ames, as well as an ML generated sales comps tools for your convenience.'''),
+        html.Div([html.Img(src='/assets/shutterstock_752498263-1400x621.jpg')], style={'textAlign': 'center','height':'10%', 'width':'10%'}))
     elif value=='tab-2':
-        return html.Div([html.H3('Lets get Ames-icable', style={'font-weight': 'bold'})]),dcc.Markdown('''Welcome to Ames, Iowa; named one of the “15 Cities That Have Done the Best Since the Recession” by Bloomberg Business in 2015 and known as the ‘healthiest city in America’ by USA Today. Home to Iowa State University, Ames has become a player in developing the world’s bio-economic future. An attractive location for business, some of the names housed here include 3M, Danfoss, Boehringer Ingelheim, and more. Below, see a simplified map of the houses sold in Ames between ____. Interacting with the map will give a general understanding of the city.'''), html.Div([html.H3('Visualize the City', style={'font-weight': 'bold'})]),dcc.Markdown('''Use this graph to visualize some of the trends of the neighborhood. Use the dropdown menu to color the points by different attributes.'''),html.Div([
-    dcc.Dropdown(
-        id='dropdown_for_realt_map',
-        options=[{'label': 'Year Built','value': 'YearBuilt'},
-        {'label': 'Gross Living Area', 'value': 'GrLivArea'} ,
-                 {'label': 'Sale Price','value': 'SalePrice'},
-
-                 {'label': 'Neighborhood','value': 'Neighborhood'},
-                 {'label': 'Overall Quality','value': 'OverallQual'}],value='colorvalue')]),dcc.Graph(id='realt_map')
+        return (html.Div([
+        html.H3('Lets get Ames-icable', style={'font-weight': 'bold'})]),
+        dcc.Markdown('''Welcome to Ames, Iowa; named one of the “15 Cities That Have Done the Best Since the Recession” by Bloomberg Business in 2015 and known as the ‘healthiest city in America’ by USA Today. Home to Iowa State University, Ames has become a player in developing the world’s bio-economic future. An attractive location for business, some of the names housed here include 3M, Danfoss, Boehringer Ingelheim, and more. Below, see a simplified map of the houses sold in Ames between ____. Interacting with the map will give a general understanding of the city.'''),
+        html.Div([html.H3('Visualize the City', style={'font-weight': 'bold'})]),
+        dcc.Markdown('''Use this graph to visualize some of the trends of the neighborhood. Use the dropdown menu to color the points by different attributes.'''),
+        html.Div([
+        dcc.Dropdown(id='dropdown_for_realt_map',options=[{'label': 'Year Built','value': 'YearBuilt'},{'label': 'Gross Living Area', 'value': 'GrLivArea'} ,{'label': 'Sale Price','value': 'SalePrice'},{'label': 'Neighborhood','value': 'Neighborhood'},{'label': 'Overall Quality','value': 'OverallQual'}],value='colorvalue')]),
+        dcc.Graph(id='realt_map'))
     elif value=='tab-5':
-        return html.Div([html.H3('KNN Sales Comps', style={'font-weight': 'bold'})]),dcc.Markdown('''This tool uses K-Nearest Neighbors to generate automatic sales comps. Input attributes below to find houses most similar to it and how much they sold for. See below for accepted ranges.'''),dcc.Input(id='grlivarea',placeholder='Enter Gross Living Area',type='text',value=''),dcc.Input(id='lotarea',placeholder='Enter Lot Area value',type='text',value=''),dcc.Input(id='qualval',placeholder='Enter Quality value 1-10',type='text',value=''),dcc.Input(id='yearbuiltval',placeholder='Enter Year Built',type='text',value=''),dcc.Input(id='bsmtsfval',placeholder='Enter Basement SF',type='text',value=''),dcc.Input(id='garageareaval',placeholder='Enter Garage Area',type='text',value=''),dcc.Input(id='latval',placeholder='Enter Latitude',type='text',value=''),dcc.Input(id='longval',placeholder='Enter Longitude',type='text',value=''),dbc.FormGroup([dbc.Label("Remodeled?"),dbc.RadioItems(options=[{"label": "True", "value": 1},{"label": "False", "value":2},],value=1,id="remodeledtf",inline=True)]),dash_table.DataTable(id='knn_table')
+        return (html.Div([
+        html.H3('KNN Sales Comps', style={'font-weight': 'bold'})]),
+        dcc.Markdown('''This tool uses K-Nearest Neighbors to generate automatic sales comps. Input attributes below to find houses most similar to it and how much they sold for. See below for accepted ranges.'''),
+        dcc.Input(id='grlivarea',placeholder='Enter Gross Living Area',type='text',value=''),
+        dcc.Input(id='lotarea',placeholder='Enter Lot Area value',type='text',value=''),
+        dcc.Input(id='qualval',placeholder='Enter Quality value 1-10',type='text',value=''),
+        dcc.Input(id='yearbuiltval',placeholder='Enter Year Built',type='text',value=''),
+        dcc.Input(id='bsmtsfval',placeholder='Enter Basement SF',type='text',value=''),
+        dcc.Input(id='garageareaval',placeholder='Enter Garage Area',type='text',value=''),
+        dcc.Input(id='latval',placeholder='Enter Latitude',type='text',value=''),
+        dcc.Input(id='longval',placeholder='Enter Longitude',type='text',value=''),
+        dbc.FormGroup([dbc.Label("Remodeled?"),
+        dbc.RadioItems(options=[{"label": "True", "value": 1},{"label": "False", "value":2},],value=1,id="remodeledtf",inline=True)]),
+        dash_table.DataTable(id='knn_table'))
 @app.callback(
     Output('realt_map', 'figure'),
     Input('dropdown_for_realt_map', 'value'))
 def return_realt_map(colorvalue):
-    tdata=pd.read_csv('apps/tdata_for_realtor_map.csv')
+    tdata=pd.read_csv('apps/data/tdata_for_realtor_map.csv')
     if colorvalue=='YearBuilt':
         return px.scatter(tdata, x="Lat", y="Long", color='YearBuilt',title="Houses Sold in Ames Iowa \'06-\'09'")
     elif colorvalue=='GrLivArea':
@@ -114,7 +128,7 @@ def return_realt_map(colorvalue):
     Input('lataval', 'value'),
     Input('longval', 'value')])
 def return_knn(valuegr,valuela,valuequality,valueyearbuilt,valuebsmtsf,valuegarage,valueremodeled,valuelat,valuelong):
-    tdata=pd.read_csv('apps/Data_For_KNN_salescomps.csv')
+    tdata=pd.read_csv('apps/data/Data_For_KNN_salescomps.csv')
     Miles=2
     numofneighbors=10
     instance=pd.DataFrame({'GrLivArea':valuegr,   'LotArea':valuela,  'OverallQual':valuequality, 'YearBuilt':valueyearbuilt,
